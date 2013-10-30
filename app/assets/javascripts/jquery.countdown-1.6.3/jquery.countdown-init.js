4bdfc1f84faacaf8b4df86688e889b3e2fce660b
Paul Sutcliffe <paul@kosmyka.com>
2013-10-29 21:44:41 -0500

    detalles

--- app/assets/javascripts/jquery.countdown-1.6.3/jquery.countdown-init.js
+++ app/assets/javascripts/jquery.countdown-1.6.3/jquery.countdown-init.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-  $('.contador').countdown({until: new Date(2013, 12-1, 06), timezone: -5, timeSeparator: ":"});
+  $('.contador').countdown({until: new Date(2013, 12-1, 06), timezone: -3, timeSeparator: ":"});
                                          // año, mes-1, día
 });
